4d77ed2efd4d12eb590e09fd8c70fed4bf2cc838
TomsFreimanis <91910277+TomsFreimanis@users.noreply.github.com>
2026-05-08 16:52:14 +0300

    init
    innitcom

--- client/src/index.js
+++ client/src/index.js
@@ -7,10 +7,9 @@ import App from './App';
 const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-      <App />
-    </GoogleOAuthProvider>
-  </React.StrictMode>
-);
+
+const tree = GOOGLE_CLIENT_ID
+  ? <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}><App /></GoogleOAuthProvider>
+  : <App />;
+
+root.render(<React.StrictMode>{tree}</React.StrictMode>);
